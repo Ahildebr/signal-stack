8ae9fcbc181b25eebe3ecd72c1efcc15beb6ae2b
Aidan Hildebrand <blackbirdgt3rs@gmail.com>
2025-05-30 10:12:05 -0700

    Minor File Restructure

---
+++ src/data/mockPosts.js
@@ -0,0 +1,34 @@
+const mockPosts = [
+  {
+    id: 1,
+    title: "Just because they value communication...",
+    views: 12840,
+    likes: 932,
+    shares: 212,
+    followers: 40,
+    postedAt: "2025-05-28 10:00",
+    vpn: "off",
+  },
+  {
+    id: 2,
+    title: "Silence is also an answer",
+    views: 8430,
+    likes: 630,
+    shares: 154,
+    followers: 29,
+    postedAt: "2025-05-29 02:00",
+    vpn: "on",
+  },
+  {
+    id: 3,
+    title: "People who laugh during arguments...",
+    views: 16810,
+    likes: 1150,
+    shares: 287,
+    followers: 64,
+    postedAt: "2025-05-30 10:00",
+    vpn: "off",
+  },
+];
+
+export default mockPosts;
